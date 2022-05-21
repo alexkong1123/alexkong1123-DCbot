92020a9a4393bc3d45bbf198f77aef581385799e
Alexkong1123 <103373823+alexkong1123@users.noreply.github.com>
2022-05-21 02:20:09 +0000

    status

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import keep_alive
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('test'):
+        await message.channel.send('testing...')
+
+status_w = discord.Status.idle
+
+client.run('OTc3Mzg2MzQwMjAxMTYwNzI0.GuzMBH.dsIMJFJpb-uC7O01qzvnTknokxEbnaK8JD4pZw')
