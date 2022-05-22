62a9cfddc666fdb3a7aef35ece6ec70bf9b7ba92
Alexkong1123 <103373823+alexkong1123@users.noreply.github.com>
2022-05-22 02:21:24 +0000

    embed test but failed

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
 import keep_alive
+from discord import Embed
+import command
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged! {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -15,13 +17,7 @@ async def on_message(message):
     if message.content.startswith('test'):
         await message.channel.send('testing....')
 
-    if message.content.startswith('test'):
-        embed = discord.Embed(
-            title = "Title",
-            description = "test",
-            colour = discord.Colour.blue()
-        )
-      
+       
 if __name__ == "__main__":
  keep_alive.keep_alive()
  client.run('OTc3Mzg2MzQwMjAxMTYwNzI0.GuzMBH.dsIMJFJpb-uC7O01qzvnTknokxEbnaK8JD4pZw')
